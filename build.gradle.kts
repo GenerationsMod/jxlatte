3dd39abd64213836c4a0078e6cfa5a906fb0bde9
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-06-10 10:47:20 -0400

    Move from Groovy to Kotlin Gradle and add Generations Repo

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    idea
+    `maven-publish`
+}
+
+group = "com.thebombzen"
+version = "1.1.0"
+
+publishing {
+    publications.create<MavenPublication>("mavenCommon") {
+        artifactId = "${project.properties["archives_base_name"]}" + "-Common"
+        from(components["java"])
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            val releasesRepoUrl = "https://maven.generations.gg/releases"
+            val snapshotsRepoUrl = "https://maven.generations.gg/snapshots"
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "Generations-Repo"
+            credentials {
+                username = project.properties["repoLogin"]?.toString()
+                password = project.properties["repoPassword"]?.toString()
+            }
+        }
+    }
+}
